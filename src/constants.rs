1992fdf00c030ec0b312d5ecca23df649800d524
Pascal Trovatelli <34663482+ptrovatelli@users.noreply.github.com>
2024-08-09 08:48:29 +0200

    fix missing food items (need to dev pagination soon!)

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub const CONNECTION_POOL_ERROR: &str = "couldn't get DB connection from pool";
-pub const MAX_FOOD_ITEMS : i64 = 50;
+pub const MAX_FOOD_ITEMS : i64 = 100;
 pub const I18N_LANGUAGES: &[&str] = &["fr"];
 
 pub const MAX_MIXES : i64 = 50;
